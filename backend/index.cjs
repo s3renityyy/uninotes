d1a07a86f6d71949ec5831ba1b89fe4d3e0a16c6
s3r <babaevamir.s3r@gmail.com>
2025-04-16 23:11:17 +0300

    feat: route crud added

--- backend/index.cjs
+++ backend/index.cjs
@@ -22,7 +22,7 @@ mongoose
   .catch((err) => console.error(err));
 
 app.use("/api", apiRoutes);
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
