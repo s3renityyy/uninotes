e07fc00c76d7e50964fc9546d67b9417902a0337
s3r <babaevamir.s3r@gmail.com>
2025-04-16 23:00:57 +0300

    feat: getting ready for deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from "vite";
+/// <reference types="vite/client" />
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:9000",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
