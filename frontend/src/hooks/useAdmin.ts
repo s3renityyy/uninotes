5338f4e506efb1a93edcb7156f04319c05398c76
s3r <babaevamir.s3r@gmail.com>
2025-04-16 23:20:27 +0300

    feat: getting ready for deploy

--- frontend/src/hooks/useAdmin.ts
+++ frontend/src/hooks/useAdmin.ts
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react";
 
-const ADMIN_PASSWORD = import.meta.env.ADMIN_KEY;
+const ADMIN_KEY = import.meta.env.VITE_ADMIN_KEY;
 
 export const useAdmin = () => {
   const [isAdmin, setIsAdmin] = useState<boolean>(false);
 
+  console.log(ADMIN_KEY);
   useEffect(() => {
     const stored = localStorage.getItem("isAdmin");
     if (stored === "true") setIsAdmin(true);
   }, []);
 
   const login = (password: string) => {
-    if (password === ADMIN_PASSWORD) {
+    if (password === ADMIN_KEY) {
       setIsAdmin(true);
       localStorage.setItem("isAdmin", "true");
       return true;
